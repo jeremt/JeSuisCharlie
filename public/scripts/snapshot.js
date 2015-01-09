a91011f61edf8e9dcf5e6604895ac19d91edbe49
Jeremie Taboada <taboada.jeremie@gmail.com>
2015-01-09 12:55:20 +0000

    Add facebook test

--- public/scripts/snapshot.js
+++ public/scripts/snapshot.js
@@ -5,21 +5,29 @@ retryBtn.addEventListener("click", function () {
     window.location.href = "/";
 });
 
-function changeProfilePicture() {
-    FB.api('/me/feed', 'post', {message: '#jeSuisCharlie'});
-}
-
 facebookBtn.addEventListener("click", function () {
-    window.alert("Not implemented yet, sorry :/")
-    ////FB.login(function(){
-    ////    console.log("TODO");
-    ////}, {scope: 'publish_actions'});
-    //FB.getLoginStatus(function(response) {
-    //    if (response.status === 'connected') {
-    //        changeProfilePicture();
+    //window.alert("Not implemented yet, sorry :/")
+
+    // test that the app is working
+    FB.login(function(){
+        FB.api('/me/feed', 'post', {message: 'Test API #jeSuisCharlie'});
+    }, {scope: 'publish_actions'});
+
+    // upload picture
+    //
+    //FB.api(
+    //    "/me/photos",
+    //    "POST",
+    //    {
+    //        "url": "http://noussommescharlie.heroku.com/images/" + currentId + ".png"
+    //    },
+    //    function (response) {
+    //        if (response && !response.error) {
+    //            console.log(response);
+    //        }
     //    }
-    //    else {
-    //        FB.login(changeProfilePicture);
-    //    }
-    //});
+    //);
+
+    // make profile picture
+    //https://www.facebook.com/photo.php?fbid={photo_id}&makeprofile=1&makeuserprofile=1
 });
